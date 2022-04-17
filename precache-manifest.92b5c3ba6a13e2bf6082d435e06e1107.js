1ca4ee2581eec588d9450329e244b9a579b1b167
anurag kanungo <anuragkanungo1818@gmail.com>
2022-04-17 18:33:26 +0530

    Updates

---
+++ precache-manifest.92b5c3ba6a13e2bf6082d435e06e1107.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d2e8a7e664a4057c33b",
+    "url": "/robotfriends/static/css/main.f0298aff.chunk.css"
+  },
+  {
+    "revision": "1d2e8a7e664a4057c33b",
+    "url": "/robotfriends/static/js/main.1d2e8a7e.chunk.js"
+  },
+  {
+    "revision": "acf36c59baacb253f049",
+    "url": "/robotfriends/static/css/1.356fee8b.chunk.css"
+  },
+  {
+    "revision": "acf36c59baacb253f049",
+    "url": "/robotfriends/static/js/1.acf36c59.chunk.js"
+  },
+  {
+    "revision": "036993615acbccafb03f",
+    "url": "/robotfriends/static/js/runtime~main.03699361.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c94e0d4106af37ae5162ca25d3863a88",
+    "url": "/robotfriends/index.html"
+  }
+];
